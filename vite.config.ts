0033b486701fd68dcaa8aebec49f65bf3f42a418
Kevin Galeazzi <galeazzikev@gmail.com>
2025-03-03 17:44:30 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/KuvsLibrary/',
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
+  base: '/KuvsLibrary/', // Make sure this matches your repo name exactly
 });
